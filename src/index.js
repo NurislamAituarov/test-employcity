ad777219e1a2f6661767bd673e82ebc878e7266b
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-09-20 13:16:27 +0600

    add burger menu

--- src/index.js
+++ src/index.js
@@ -23,3 +23,22 @@ on('click', '.option', (item) => {
   parent.setAttribute('data-type', item.target.getAttribute('data-type'));
   parent.innerText = item.target.innerText;
 });
+
+// Burger menu //
+
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.header__nav-mobile');
+
+burger.addEventListener('click', () => {
+  burger.classList.remove('active-menu');
+  close.classList.add('active-menu');
+  nav.classList.add('header__nav-mobile-active');
+  document.body.style.overflow = 'hidden';
+});
+close.addEventListener('click', () => {
+  close.classList.remove('active-menu');
+  burger.classList.add('active-menu');
+  nav.classList.remove('header__nav-mobile-active');
+  document.body.style.overflow = 'visible';
+});
